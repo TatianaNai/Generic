f23b036bc6c69df5123f385435a281ab7b107cd7
Tatiana <134440396+TatianaNai@users.noreply.github.com>
2024-04-28 11:13:11 +0300

    Add files via upload

---
+++ Generic/ComparablePair.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class ComparablePair <T, U>  : IComparable<ComparablePair<T,U>>
+                                        where T : IComparable<T>
+                                        where U : IComparable<U>
+    {
+        public T ParametrT { get; }
+        public U ParametrU { get; }
+
+        public ComparablePair(T parametrT, U parametrU)
+        {
+            ParametrT = parametrT;
+            ParametrU = parametrU;
+        }
+
+        public int CompareTo(ComparablePair<T, U>? other)
+        {
+            int resultOfComparisonFirstItems = ParametrT.CompareTo(other.ParametrT);
+            int resultOfComparisonSecondItems = ParametrU.CompareTo(other.ParametrU);
+
+            return resultOfComparisonFirstItems == 0 ? resultOfComparisonSecondItems : resultOfComparisonFirstItems;
+        }
+    }
+}
---
+++ Generic/Pair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Pair <S, T>
+    {
+        public S Item1 { get; }
+        public T Item2 { get; }
+
+        public Pair(S item1, T item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+}
